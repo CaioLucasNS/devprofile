1be63133f41bc38226a58cab39bdbeb0e816fdb8
Caio Lucas <lcaio1281@gmail.com>
2023-04-06 17:37:28 -0300

    feat: Header component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  Icon,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+} from './styles';
+
+import avatarDefault from '../../assets/avatar02.png';
 
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Title>Home page</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+
+            <UserInfoDetail>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Caio</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
