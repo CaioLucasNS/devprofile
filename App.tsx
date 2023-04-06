795fdeffb0d6f556ee9cc3890bebe8bf8ac1808f
Caio Lucas <lcaio1281@gmail.com>
2023-04-06 16:14:51 -0300

    configured external font in global theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/style/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
 import { Home } from './src/pages/Home';
 
+import AppLoading from 'expo-app-loading';
+
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
