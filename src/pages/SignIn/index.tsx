c6acf77fced0178158225a6c1873413bd201482d
Caio <lcaio1281@gmail.com>
2023-04-07 20:19:02 -0300

    CreateAccountButton component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {
   Container,
   Content,
+  CreateAccountButton,
+  CreateAccountTitle,
   DevProfileLogo,
   ForgotPasswordButton,
   ForgotPasswordTitle,
+  Icon,
   Title,
 } from './styles';
 import { Input } from '../../components/Form/Input';
@@ -15,25 +18,32 @@ import devProfileLogo from '../../assets/logo.png';
 
 export const SignIn: React.FunctionComponent = () => {
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
-    >
-      <Container>
-        <Content>
-          <DevProfileLogo source={devProfileLogo} />
-          <Title>Faça seu login</Title>
+    <>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <DevProfileLogo source={devProfileLogo} />
+            <Title>Faça seu login</Title>
 
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
+            <Input placeholder="Email" />
+            <Input placeholder="Senha" />
 
-          <Button title="Entrar" />
+            <Button title="Entrar" />
 
-          <ForgotPasswordButton>
-            <ForgotPasswordTitle>Esqueci minha senha</ForgotPasswordTitle>
-          </ForgotPasswordButton>
-        </Content>
-      </Container>
-    </ScrollView>
+            <ForgotPasswordButton>
+              <ForgotPasswordTitle>Esqueci minha senha</ForgotPasswordTitle>
+            </ForgotPasswordButton>
+          </Content>
+        </Container>
+      </ScrollView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" />
+        <CreateAccountTitle>Criar uma conta</CreateAccountTitle>
+      </CreateAccountButton>
+    </>
   );
 };
