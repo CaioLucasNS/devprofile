c46ce7457e6c97866469851eabab7fe8e523acb8
Caio <lcaio1281@gmail.com>
2023-04-08 16:42:04 -0300

    add error message in InputControl component

--- src/components/Form/InputControl/index.tsx
+++ src/components/Form/InputControl/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
 import { Control, Controller } from 'react-hook-form';
-import { Container } from './styles';
+import { Container, ErrorMessageText } from './styles';
 import { Input } from '../Input';
 
 interface InputControlProps extends TextInputProps {
   control: Control;
   name: string;
+  error: string | undefined;
 }
 
 export const InputControl: React.FunctionComponent<InputControlProps> = ({
   control,
   name,
+  error,
   ...rest
 }) => {
   return (
@@ -24,6 +26,7 @@ export const InputControl: React.FunctionComponent<InputControlProps> = ({
         )}
         name={name}
       />
+      {error && <ErrorMessageText>{error}</ErrorMessageText>}
     </Container>
   );
 };
